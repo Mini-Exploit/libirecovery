ee3a537b3a00b4d3b6ab66823657480361087688
mini_exploit <61931266+Mini-Exploit@users.noreply.github.com>
2022-03-02 14:59:16 +0700

    Force print the device info

--- tools/irecovery.c
+++ tools/irecovery.c
@@ -180,11 +180,12 @@ static void print_device_info(irecv_client_t client)
 	}
 
 	irecv_devices_get_device_by_client(client, &device);
-	if (device) {
-		printf("PRODUCT: %s\n", device->product_type);
-		printf("MODEL: %s\n", device->hardware_model);
-		printf("NAME: %s\n", device->display_name);
-	}
+	
+	// Force print the device info
+	printf("PRODUCT: %s\n", device->product_type);
+	printf("MODEL: %s\n", device->hardware_model);
+	printf("NAME: %s\n", device->display_name);
+
 }
 
 static void print_devices() {
